e3fa279d7186a992ec6db4513969396875f14a30
taqui-786 <mdtaqui.jhar@gmail.com>
2026-01-19 22:14:32 +0530

    Fix: image size issue

--- components/HeroImageSection.tsx
+++ components/HeroImageSection.tsx
@@ -5,7 +5,7 @@ function HeroImageSection() {
   return (
     <div className="relative mb-26 animate-fade-in-up">
       <div className="p-1  border border-dashed dark:border-white/30 border-black/20 rounded-2xl size-auto">
-        <div className="h-76 w-full relative  overflow-hidden rounded-xl border border-border">
+        <div className="md:h-76 h-64 w-full relative  overflow-hidden rounded-xl border border-border aspect-video">
           <Image
             src={"/testBannerHero3.jpg"}
             alt="Taqui Imam"
@@ -21,7 +21,7 @@ function HeroImageSection() {
       {/* Profile Image */}
       <div className="absolute -bottom-16 md:left-8 left-4 z-40 group">
         <div className="relative">
-          <div className="relative h-40 w-40 rounded-full overflow-hidden backdrop-blur-xl bg-white/20 dark:bg-white/10 border border-white/30 dark:border-white/20 shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] group-hover:shadow-[0_8px_32px_0_rgba(31,38,135,0.5)] transition-all duration-300 group-hover:scale-105">
+          <div className="relative md:size-40 size-36 rounded-full overflow-hidden backdrop-blur-xl bg-white/20 dark:bg-white/10 border border-white/30 dark:border-white/20 shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] group-hover:shadow-[0_8px_32px_0_rgba(31,38,135,0.5)] transition-all duration-300 group-hover:scale-105">
             <div className="absolute inset-0 bg-linear-to-br from-white/30 via-white/10 to-transparent pointer-events-none"></div>
             <Image
               className="
